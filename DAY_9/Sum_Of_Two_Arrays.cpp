55adf368b8a03401827521609d90343ba406770c
lakshayjuneja003 <lakshayjuneja003@gmail.com>
2025-03-27 11:04:06 -0400

    Arrays Cpp

---
+++ DAY_9/Sum_Of_Two_Arrays.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h> 
+vector<int> reverse(vector<int> v){
+	int s = 0;
+	int e = v.size() -1;
+	while( s <= e){
+		swap(v[s++] , v[e--]);
+	}
+	return v;
+}
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	int i = n-1;
+	int j = m-1;
+	vector<int> ans;
+	int carry = 0;
+
+	while(i>=0 && j>= 0){
+		int val1 = a[i];
+		int val2 = b[j];
+
+		int sum = val1 + val2 + carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+		i--;
+		j--;
+	}
+
+
+	// if left part of a is left 
+	while(i>=0){
+		int val1 = a[i];
+		int sum = val1 + carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+		i--;
+	}
+	while(j>=0){
+		int val1 = b[j];
+		int sum = val1 + carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+		j--;
+	}
+
+	while(carry != 0){
+		int sum = carry;
+		carry = sum/10;
+		sum = sum%10;
+		ans.push_back(sum);
+	}
+	return reverse(ans);
+}
