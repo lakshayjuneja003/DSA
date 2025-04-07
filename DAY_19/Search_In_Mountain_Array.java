b8439eb053231e4383cee940c4137d3b62b1d45d
lakshayjuneja <lakshayjuneja003@gmail.com>
2025-04-07 20:20:52 +0530

    Leetcode Hard Question BS

---
+++ DAY_19/Search_In_Mountain_Array.java
@@ -0,0 +1,26 @@
+package DAY_19;
+
+public /**
+* // This is MountainArray's API interface.
+* // You should not implement it, or speculate about its implementation
+* interface MountainArray {
+*     public int get(int index) {}
+*     public int length() {}
+* }
+*/
+
+class Solution {
+   public int findInMountainArray(int target, MountainArray mountainArr) {
+       
+   }
+   int search(int[] arr, int target) {
+   }
+
+   public int peakIndexInMountainArray(int[] arr) {
+   }
+
+   static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+   }
+} {
+    
+}
